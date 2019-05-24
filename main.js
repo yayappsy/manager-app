f5c9ad030b2561107d68217db86e38fe17acd9ca
chenwenlong <942057398@qq.com>
2019-05-24 16:34:29 +0800

    添加内容

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 
+import pageHead from './components/page-head.vue'
+import pageFoot from './components/page-foot.vue'
+
 Vue.config.productionTip = false
+
+Vue.component('page-head', pageHead)
+Vue.component('page-foot', pageFoot)
 
 App.mpType = 'app'
 
--- pages/self/index.vue
+++ pages/self/index.vue
@@ -1,42 +1,72 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+	<view>
+		<uni-fab :pattern="pattern" :content="content" :horizontal="horizontal" :vertical="vertical" :direction="direction"
+				 @trigger="trigger" ref="fab"></uni-fab>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+    import uniIcon from '@/components/uni-icon/uni-icon.vue';
+    import uniFab from '@/components/uni-fab.vue';
+    export default {
+        data() {
+            return {
+                horizontal: 'left',
+                vertical: 'bottom',
+                direction: 'horizontal',
+                pattern: {
+                    color: '#7A7E83',
+                    backgroundColor: '#fff',
+                    selectedColor: '#007AFF',
+                    buttonColor: '#007AFF'
+                },
+                content: [{
+                    iconPath: '/static/png/tabBar/team1.png',
+                    selectedIconPath: '/static/png/tabBar/team2.png',
+                    text: '组件',
+                    active: false
+                },
+                    {
+                        iconPath: '/static/api.png',
+                        selectedIconPath: '/static/apiHL.png',
+                        text: 'API',
+                        active: false
+                    },
+                    {
+                        iconPath: '/static/template.png',
+                        selectedIconPath: '/static/templateHL.png',
+                        text: '模版',
+                        active: false
+                    }
+                ]
+            };
+        },
+        methods: {
+            trigger(e) {
+                console.log(e);
+                this.content[e.index].active = !e.item.active;
+                uni.showModal({
+                    title: '提示',
+                    content: `您${this.content[e.index].active ? '选中了' : '取消了'}${e.item.text}`,
+                    success: function(res) {
+                        if (res.confirm) {
+                            console.log('用户点击确定');
+                        } else if (res.cancel) {
+                            console.log('用户点击取消');
+                        }
+                    }
+                });
+            },
+        },
+        components: {
+            uniFab,
+            uniIcon
+        }
+    };
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	.btn {
+		margin: 30upx 0;
 	}
 </style>
